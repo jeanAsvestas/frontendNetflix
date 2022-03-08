04687fcb5707402514cf5e58dfbff20fcf2876a5
ELa <nikos_christofil@gmail.com>
2022-03-08 23:17:22 +0200

    fdf

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,9 +1,29 @@
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@material-ui/icons";
 import "./listItem.scss"
 
 export default function ListItem() {
   return (
     <div className="listItem">
-        Item
+        <img src="https://www.homecinemachoice.com/images/styles/600_wide/public/scarface%2001.jpg" alt="" />
+
+    <div className="itemInfo">
+      <div className="icons">
+          <PlayArrow/>
+          <Add/>
+          <ThumbUpAltOutlined/>
+          <ThumbDownAltOutlined/>
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour,42 minutes</span>
+          <span>18+</span>
+          <span>2005</span>
+          </div>
+          <div className="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            ratione voluptatem, qui consequatur officia?
+            </div>
+            <div className="genre">Action</div>
+      </div>
     </div>
-  )
+  );
 }
